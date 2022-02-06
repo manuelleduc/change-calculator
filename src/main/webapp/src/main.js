066f2611c12cab4554849739dcef50749c12302d
Manuel Leduc <manuel.leduc@xwiki.com>
2022-02-06 22:26:09 +0100

    Improve debug config

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -1,11 +1,5 @@
-import Vue, {createApp} from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-
-if (process.env.NODE_ENV === 'development') {
-  Vue.config.devtools = true
-  Vue.config.performance = true
-  Vue.config.productionTip = false
-}
 
 createApp(App).mount('#app')
 
--- src/main/webapp/vue.config.js
+++ src/main/webapp/vue.config.js
@@ -1,6 +1,13 @@
+const webpack = require('webpack');
+
 module.exports = {
   chainWebpack: config => {
     config.resolve.alias.set('vue', '@vue/compat')
+
+    config.plugin('feature-flags').tap(args => {
+      args[0].__VUE_PROD_DEVTOOLS__ = JSON.stringify(true)
+      return args
+    })
 
     config.module
       .rule('vue')
